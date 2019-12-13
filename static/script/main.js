0ca176fbf0d4740e44c93d95d09ddcfe0f147f38
Chiangwanyi <chiangwanyi@163.com>
2019-12-13 14:23:24 +0800

    v1.0.0

--- static/script/main.js
+++ static/script/main.js
@@ -1,7 +1,8 @@
 const urls = [
     "http://192.168.1.156:8080",
     "http://localhost:8080",
-    "http://10.118.143.51:8080"
+    "http://10.118.143.51:8080",
+    "http://192.168.43.58:8080"
 ];
 
 const back_end_url = urls[0];
